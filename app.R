b821d194406c17f2c7de539f6c27ac77e71d0e6f
Fernando Cutire <fernandocutire@gmail.com>
2021-02-12 10:58:42 -0500

    Dashboard modified:

--- app.R
+++ app.R
@@ -11,26 +11,32 @@ ui <- fluidPage(
     sidebarPanel(
       img(src = "/mibus.png", height = 140, width = 200),
       
-      h2("Meta"),
-      helpText("Mide el tráfico en la actual ciudad de Panamá"),
-      
-      sliderInput("year",
-                  "Year",
-                  min = 2021,
-                  max = 2040,
-                  step = 1,
-                  sep = "",
-                  value = 2022),
+      h2("Objetivo General"),
+      helpText("Desarrollar un modelo de monitoreo vehicular inteligente en tiempo real para mejorar la distribución y logística del transporte panameño."),
+      #sliderInput("year",
+      #            "Año",
+      #            min = 2021,
+      #            max = 2040,
+      #            step = 1,
+      #            sep = "",
+      #            value = 2022),
       selectInput("select", h3("Selecciona una carretera"), 
                   choices = list("Carretera Transístmica" = 1, "Vía Ricardo J. Alfaro" = 2,
-                                 "Carretera Panamericana" = 3), selected = 1)
+                                 "Carretera Panamericana" = 3), selected = 1),
+
+      dateInput("date", h3("Indique un día"), value = "2021-02-12"),
+      sliderInput("slider1", h3("Seleccione una hora"),
+                  min = 0, max = 24, value = 7)
     
     ),
     mainPanel(
       h1("Tráfico actual "),
       img(src = "/panamaCentro.png", height = 500, width = 900),
-      p("Gobierno Digital conformado por: Prof: Juan Saldaña, Prof: Víctor López, Valerie Tuñón, Anel Atencio, Fernando Cutire")
-      
+      fluidRow(
+        column(2, "Hola"),
+        column(4, 'Hey')
+        ),
+      strong("Desarrollado por el grupo Gobierno Digital conformado por: Prof: Juan Saldaña, Prof: Víctor López, Valerie Tuñón, Anel Atencio, Fernando Cutire")
     )
   )
 )
